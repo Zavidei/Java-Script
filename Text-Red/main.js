41098c512dff4c38ae02af2f2fa27ceab6b8a7c2
Завидей Дмитрий <144597993+Zavidei@users.noreply.github.com>
2024-10-17 10:30:51 +0300

    update text editor 2

--- Text-Red/main.js
+++ Text-Red/main.js
@@ -1,25 +1,37 @@
 class textRedactor {
-    constructor(textSpace, buttonTextEditor, fontSelector, fontSizeInput) {
+    constructor(textSpace, buttonTextEditor, fontSelector, fontSizeInput, boldButton, italicButton, underlineButton, colorText, colorBackground, uppercaseButton, lowercaseButton) {
         this.textSpace = textSpace;
         this.buttonTextEditor = buttonTextEditor;
         this.fontSelector = fontSelector;
-        this.currentFont = "Arial";
         this.fontSizeInput = fontSizeInput;
+        this.boldButton = boldButton;
+        this.italicButton = italicButton;
+        this.underlineButton = underlineButton;
+        this.colorText = colorText;
+        this.colorBackground = colorBackground;
+        this.uppercaseButton = uppercaseButton;
+        this.lowercaseButton = lowercaseButton;
+        
+        this.currentFont = "Arial";
         this.currentFontSize = 12;
-        this.fontSelector.addEventListener("change", () => {
-            this.changeFontFamalyTextInDocument();
-        });
-        this.fontSizeInput.addEventListener("change", () => {
-            this.changeFontSizeTextInDocument();
 
-        });
-
+        this.addEventListeners();
     }
 
-    
+    addEventListeners() {
+        this.fontSelector.addEventListener("change", () => this.changeFontFamalyTextInDocument());
+        this.fontSizeInput.addEventListener("change", () => this.changeFontSizeTextInDocument());
+        this.boldButton.addEventListener("click", () => this.toggleBoldText());
+        this.italicButton.addEventListener("click", () => this.toggleItalicText());
+        this.underlineButton.addEventListener("click", () => this.toggleUnderlineText());
+        this.colorText.addEventListener("input", () => this.changeTextColor());
+        this.colorBackground.addEventListener("input", () => this.changeBackgroundColor());
+        this.uppercaseButton.addEventListener("click", () => this.changeToUppercase());
+        this.lowercaseButton.addEventListener("click", () => this.changeToLowercase());
+    }
 
     saveFile() {
-        const text = this.textSpace.innerText;
+        const text = this.textSpace.innerHTML;
         const blob = new Blob([text], { type: "application/msword" });
         const link = document.createElement("a");
 
@@ -39,8 +51,8 @@ class textRedactor {
                 const reader = new FileReader();
                 reader.readAsText(file);
                 reader.onload = () => {
-                    this.textSpace.innerText = reader.result;
-                }
+                    this.textSpace.innerHTML = reader.result;
+                };
             }
         });
     }
@@ -60,26 +72,59 @@ class textRedactor {
         });
     }
 
-    changeFontFamalyTextInDocument () {
+    changeFontFamalyTextInDocument() {
+        this.applyStyleToSelection("fontFamily", this.fontSelector.value);
+    }
+
+    changeFontSizeTextInDocument() {
+        this.applyStyleToSelection("fontSize", this.fontSizeInput.value);
+    }
+
+    toggleBoldText() {
+        document.execCommand("bold");
+    }
+
+    toggleItalicText() {
+        document.execCommand("italic");
+    }
+
+    toggleUnderlineText() {
+        document.execCommand("underline");
+    }
+
+    changeTextColor() {
+        this.applyStyleToSelection("color", this.colorText.value);
+    }
+
+    changeBackgroundColor() {
+        this.applyStyleToSelection("backgroundColor", this.colorBackground.value);
+    }
+
+    changeToUppercase() {
+        this.changeTextCase("uppercase");
+    }
+
+    changeToLowercase() {
+        this.changeTextCase("lowercase");
+    }
+
+    changeTextCase(caseType) {
         const selection = window.getSelection();
         if (selection.rangeCount > 0) {
             const range = selection.getRangeAt(0);
-            const font = this.fontSelector.value;
-            const span = document.createElement("span");
-            span.style.fontFamily = font; 
-
-            range.surroundContents(span);
+            const text = range.toString();
+            const transformedText = caseType === "uppercase" ? text.toUpperCase() : text.toLowerCase();
+            
+            document.execCommand("insertText", false, transformedText);
         }
-    }  
-    
-    changeFontSizeTextInDocument () {
-        const input = window.getSelection();
-        if (input.rangeCount > 0) {
-            const range = input.getRangeAt(0);
-            const size = this.fontSizeInput.value;
-            const span = document.createElement("span");
-            span.style.fontSize = size;
+    }
 
+    applyStyleToSelection(style, value) {
+        const selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            const range = selection.getRangeAt(0);
+            const span = document.createElement("span");
+            span.style[style] = value;
             range.surroundContents(span);
         }
     }
@@ -89,7 +134,18 @@ const textSpace = document.querySelector(".textArea");
 const buttonTextEditor = document.querySelectorAll(".button");
 const fontSelector = document.querySelector(".fontsFamily");
 const fontSizeInput = document.querySelector(".fontSize");
+const boldButton = document.querySelector(".bolt");
+const italicButton = document.querySelector(".cursiv");
+const underlineButton = document.querySelector(".line");
+const colorText = document.querySelector(".colorText");
+const colorBackground = document.querySelector(".colorBackground");
+const uppercaseButton = document.querySelector(".registorHight");
+const lowercaseButton = document.querySelector(".registor");
 
-const textEditor = new textRedactor(textSpace, buttonTextEditor, fontSelector, fontSizeInput);
+const textEditor = new textRedactor(
+    textSpace, buttonTextEditor, fontSelector, fontSizeInput, 
+    boldButton, italicButton, underlineButton, 
+    colorText, colorBackground, uppercaseButton, lowercaseButton
+);
 
 textEditor.pressToButton();
